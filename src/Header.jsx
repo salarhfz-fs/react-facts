338e216cfb1d81608a1c94d338dd14c28f8e8195
Salar Hafezi <salar.hfz.work@gmail.com>
2023-01-24 13:32:54 +0330

    add header

---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+import reactLogo from './assets/react-logo.png';
+
+function Header() {
+    return (
+        <img alt='React logo' src={reactLogo} width={40} height={40} />
+    );
+}
+
+export default Header;
